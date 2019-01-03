a46b52a45ea852e103cebb1a4a46b16c571db084
zhouyer <419547093@qq.com>
2019-01-04 00:53:46 +0800

    create ssm project

---
+++ src/main/java/com/zhouyer/controller/IndexController.java
@@ -0,0 +1,11 @@
+package com.zhouyer.controller;
+
+/**
+ * @ClassName IndexController
+ * @Description TODO
+ * @Author zhouyer
+ * @Date 2019/1/4 0:03
+ * @Version 1.0
+ **/
+public class IndexController {
+}
